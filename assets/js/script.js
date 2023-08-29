3587fd071b49cca60f2f7dd8d7b5ae66d1937a31
manuumqm <manuelamachado@aluno.senai.br>
2023-08-29 09:31:38 -0300

    feat: function envieMensagem

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,21 +13,20 @@ function verificarInputs() {
     console.log(Foto);
     console.log(Data);
 
-    
+
     // verificacao se os inputs estao vazios.
     if (tutor == "" || nomePet == "" || Especie == "" || Foto == "" || Data == "") {
-        //Verificar se o if esta funcionando
         //console.log("Os inputs estao vazios.");
         envieMensagem('Preencha todos os campos', 'erro');
         return true;
     } else {
-       // console.log("Os inputs estao preenchidos.");
+        // console.log("Os inputs estao preenchidos.");
         envieMensagem('Cadastrado com sucesso', 'sucesso');
         return false;
     }
 }
 
-function envieMsg(msg, tipo) {
+function envieMensagem(msg, tipo) {
     //Essa funcao vai colocar uma msg que vem pelo parametro na tela do computador
 
     let msgDiv = document.getElementById("msg");
@@ -51,5 +50,38 @@ class CadastroPet {
         this.nomePet = nomePet;
         this.Especie = Especie;
         this.Foto = Foto;
+    }
+}
+
+function RegistroPets(){
+    // Para compor meu JOGO eu preciso pegar os valores dos inputs.
+    // Para isso, eu preciso criar uma variavel para cada input.
+
+    let tutor = document.getElementById("input-Tutor").value;
+    let nomePet = document.getElementById("input-Pet").value;
+    let Especie = document.getElementById("input-Especie").value;
+    let Foto = document.getElementById("input-Foto").value;
+    let Data = document.getElementById("input-Data").value;
+
+
+    const  CadastroPet = new CadastroPet(tutor, nomePet, Especie, Foto, Data);
+
+    ListaPets.add(nomePet);
+    console.log(ListaPets);
+}
+
+class ListaPets {
+    constructor() {
+        this.ListaPets = [];
+    
+
+    if (verificarInputs()) {
+        envieMensagem("Preencha todos os campos", "erro");
+    } else {
+        this.ListaPets.push(parametro);
+        limparInputs();
+        envieMensagem("Cadastrado com sucesso", "sucesso")
+        console.log(this.ListaPets);
+    }
     }
 }
